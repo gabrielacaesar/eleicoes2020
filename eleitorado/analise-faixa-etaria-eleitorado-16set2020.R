fcbfb84c3233ee32f4edb3d255f41ac19c889898
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-09-18 18:31:39 -0300

    Update analise-faixa-etaria-eleitorado-16set2020.R

--- eleitorado/analise-faixa-etaria-eleitorado-16set2020.R
+++ eleitorado/analise-faixa-etaria-eleitorado-16set2020.R
@@ -4,15 +4,6 @@ library(data.table)
 getwd()
 setwd("~/Downloads/perfil_eleitorado_total/")
 
-# atual
-eleitorado_atual <- fread("perfil_eleitorado_ATUAL.csv", encoding = "Latin-1")
-
-eleitorado_atual_n <- eleitorado_atual %>%
-  select(SG_UF, NM_MUNICIPIO, DS_FAIXA_ETARIA, QT_ELEITORES_PERFIL) %>%
-  group_by(DS_FAIXA_ETARIA) %>%
-  summarise(qt_eleitores = sum(QT_ELEITORES_PERFIL)) %>%
-  mutate(df = "atual")
-
 # 2020
 eleitorado_2020 <- fread("perfil_eleitorado_2020.csv", encoding = "Latin-1")
 
@@ -59,7 +50,7 @@ eleitorado_2012_n <- eleitorado_2012 %>%
   group_by(DS_FAIXA_ETARIA) %>%
   summarise(qt_eleitores = sum(QT_ELEITORES_PERFIL)) %>%
   mutate(df = "2012")
-  
+
 # 2010
 eleitorado_2010 <- fread("perfil_eleitorado_2010.txt", encoding = "Latin-1")
 
@@ -70,15 +61,119 @@ eleitorado_2010_n <- eleitorado_2010 %>%
   mutate(df = "2010") %>%
   rename("DS_FAIXA_ETARIA" = V7)
 
+# 2008 
+eleitorado_2008 <- fread("perfil_eleitorado_2008.txt", encoding = "Latin-1")
+
+eleitorado_2008_n <- eleitorado_2008 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "2008") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 2006
+eleitorado_2006 <- fread("perfil_eleitorado_2006.txt", encoding = "Latin-1")
+
+eleitorado_2006_n <- eleitorado_2006 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "2006") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 2004 
+eleitorado_2004 <- fread("perfil_eleitorado_2004.txt", encoding = "Latin-1")
+
+eleitorado_2004_n <- eleitorado_2004 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "2004") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 2002
+eleitorado_2002 <- fread("perfil_eleitorado_2002.txt", encoding = "Latin-1")
+
+eleitorado_2002_n <- eleitorado_2002 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "2002") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 2000
+eleitorado_2000 <- fread("perfil_eleitorado_2000.txt", encoding = "Latin-1")
+
+eleitorado_2000_n <- eleitorado_2000 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "2000") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 1998
+eleitorado_1998 <- fread("perfil_eleitorado_1998.txt", encoding = "Latin-1")
+
+eleitorado_1998_n <- eleitorado_1998 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "1998") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 1996
+eleitorado_1996 <- fread("perfil_eleitorado_1996.txt", encoding = "Latin-1")
+
+eleitorado_1996_n <- eleitorado_1996 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "1996") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
+# 1994
+eleitorado_1994 <- fread("perfil_eleitorado_1994.txt", encoding = "Latin-1")
+
+eleitorado_1994_n <- eleitorado_1994 %>%
+  select(V2, V3, V7, V9) %>%
+  group_by(V7) %>%
+  summarise(qt_eleitores = sum(V9)) %>%
+  mutate(df = "1994") %>%
+  rename("DS_FAIXA_ETARIA" = V7)
+
 # binding dfs
-eleitorado <- eleitorado_atual_n %>%
-  rbind(eleitorado_2010_n) %>%
-  rbind(eleitorado_2012_n) %>%
-  rbind(eleitorado_2014_n) %>%
-  rbind(eleitorado_2016_n) %>%
+eleitorado_1994_2020 <- eleitorado_2020_n %>%
   rbind(eleitorado_2018_n) %>%
-  rbind(eleitorado_2020_n)
+  rbind(eleitorado_2016_n) %>%
+  rbind(eleitorado_2014_n) %>%
+  rbind(eleitorado_2012_n) %>%
+  rbind(eleitorado_2010_n) %>%
+  rbind(eleitorado_2008_n) %>%
+  rbind(eleitorado_2006_n) %>%
+  rbind(eleitorado_2004_n) %>%
+  rbind(eleitorado_2002_n) %>%
+  rbind(eleitorado_2000_n) %>%
+  rbind(eleitorado_1998_n) %>%
+  rbind(eleitorado_1996_n) %>%
+  rbind(eleitorado_1994_n)
 
-# downloading 
-write.csv(eleitorado, "eleitorado.csv")
+####### 
+# 2020 - UFs
 
+UFs_2020 <- eleitorado_2020 %>%
+  select(SG_UF, NM_MUNICIPIO, DS_FAIXA_ETARIA, QT_ELEITORES_PERFIL) %>%
+  group_by(SG_UF, DS_FAIXA_ETARIA) %>%
+  summarise(soma = sum(QT_ELEITORES_PERFIL)) %>%
+  pivot_wider(names_from = DS_FAIXA_ETARIA, values_from = soma)
+
+write.csv(UFs_2020, "UFs_2020.csv")
+
+# 2020 - cidades
+
+cidades_2020 <- eleitorado_2020 %>%
+  select(SG_UF, NM_MUNICIPIO, DS_FAIXA_ETARIA, QT_ELEITORES_PERFIL) %>%
+  group_by(SG_UF, NM_MUNICIPIO, DS_FAIXA_ETARIA) %>%
+  summarise(soma = sum(QT_ELEITORES_PERFIL)) %>%
+  pivot_wider(names_from = DS_FAIXA_ETARIA, values_from = soma)
+
+write.csv(cidades_2020, "cidades_2020.csv")
