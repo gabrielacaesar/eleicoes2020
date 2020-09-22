fee74e08626ddf6a310b798cc5b1e4275d55c3f1
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-09-22 20:31:17 -0300

    Add files via upload

---
+++ eleitorado/eleitorado_genero.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(data.table)
+
+### 2020
+eleitorado_2020 <- fread("C:/Users/acaesar/Downloads/eleitorado/perfil_eleitorado_2020.csv", 
+                         encoding = "Latin-1")
+
+eleitorado_2020_n <- eleitorado_2020 %>%
+  group_by(SG_UF, NM_MUNICIPIO, DS_GENERO) %>%
+  summarise(total = sum(QT_ELEITORES_PERFIL)) %>%
+  pivot_wider(names_from = DS_GENERO, values_from = total) %>%
+  replace(is.na(.), 0) %>%
+  janitor::clean_names() %>%
+  mutate(total = feminino + masculino + nao_informado,
+         fem_perc = (feminino / total) * 100,
+         mas_perc = (masculino / total) * 100,
+         diferenca = feminino - masculino,
+         mais_mulher = feminino > masculino) %>%
+  arrange(desc(fem_perc)) %>%
+  # filter(mais_mulher == TRUE)
+
+### 2016
+eleitorado_2016 <- fread("C:/Users/acaesar/Downloads/eleitorado/perfil_eleitorado_2016.txt", 
+                         encoding = "Latin-1")
+
+eleitorado_2016_n <- eleitorado_2016 %>%
+  rename(SG_UF = V2, NM_MUNICIPIO = V3, DS_GENERO = V6, QT_ELEITORES_PERFIL = V9) %>%
+  filter(SG_UF != "ZZ" & SG_UF != "DF") %>%
+  filter(NM_MUNICIPIO != "FERNANDO DE NORONHA") %>%
+  group_by(SG_UF, NM_MUNICIPIO, DS_GENERO) %>%
+  summarise(total = sum(QT_ELEITORES_PERFIL)) %>%
+  pivot_wider(names_from = DS_GENERO, values_from = total) %>%
+  replace(is.na(.), 0) %>%
+  janitor::clean_names() %>%
+  mutate(total = feminino + masculino + nao_informado,
+         fem_perc = (feminino / total) * 100,
+         mas_perc = (masculino / total) * 100,
+         diferenca = feminino - masculino,
+         mais_mulher = feminino > masculino) %>%
+  arrange(desc(fem_perc)) %>%
+  # filter(mais_mulher == TRUE)
