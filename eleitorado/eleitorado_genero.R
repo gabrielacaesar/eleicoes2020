3726425683a452cc709bb91392c970e07f20a538
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-09-23 17:43:44 -0300

    Update eleitorado_genero.R

--- eleitorado/eleitorado_genero.R
+++ eleitorado/eleitorado_genero.R
@@ -6,17 +6,17 @@ eleitorado_2020 <- fread("C:/Users/acaesar/Downloads/eleitorado/perfil_eleitorad
                          encoding = "Latin-1")
 
 eleitorado_2020_n <- eleitorado_2020 %>%
-  group_by(SG_UF, NM_MUNICIPIO, DS_GENERO) %>%
+  group_by(SG_UF, CD_MUNICIPIO, NM_MUNICIPIO, DS_GENERO) %>%
   summarise(total = sum(QT_ELEITORES_PERFIL)) %>%
   pivot_wider(names_from = DS_GENERO, values_from = total) %>%
   replace(is.na(.), 0) %>%
   janitor::clean_names() %>%
   mutate(total = feminino + masculino + nao_informado,
-         fem_perc = (feminino / total) * 100,
-         mas_perc = (masculino / total) * 100,
+         fem_perc = round((feminino / total) * 100),
+         mas_perc = round((masculino / total) * 100),
          diferenca = feminino - masculino,
          mais_mulher = feminino > masculino) %>%
-  arrange(desc(fem_perc)) %>%
+  arrange(desc(fem_perc)) 
   # filter(mais_mulher == TRUE)
 
 ### 2016
@@ -24,18 +24,24 @@ eleitorado_2016 <- fread("C:/Users/acaesar/Downloads/eleitorado/perfil_eleitorad
                          encoding = "Latin-1")
 
 eleitorado_2016_n <- eleitorado_2016 %>%
-  rename(SG_UF = V2, NM_MUNICIPIO = V3, DS_GENERO = V6, QT_ELEITORES_PERFIL = V9) %>%
+  rename(SG_UF = V2, NM_MUNICIPIO = V3, CD_MUNICIPIO = V4, 
+         DS_GENERO = V6, QT_ELEITORES_PERFIL = V9) %>%
   filter(SG_UF != "ZZ" & SG_UF != "DF") %>%
   filter(NM_MUNICIPIO != "FERNANDO DE NORONHA") %>%
-  group_by(SG_UF, NM_MUNICIPIO, DS_GENERO) %>%
+  group_by(SG_UF, CD_MUNICIPIO, NM_MUNICIPIO, DS_GENERO) %>%
   summarise(total = sum(QT_ELEITORES_PERFIL)) %>%
   pivot_wider(names_from = DS_GENERO, values_from = total) %>%
   replace(is.na(.), 0) %>%
   janitor::clean_names() %>%
   mutate(total = feminino + masculino + nao_informado,
-         fem_perc = (feminino / total) * 100,
-         mas_perc = (masculino / total) * 100,
+         fem_perc = round((feminino / total) * 100),
+         mas_perc = round((masculino / total) * 100),
          diferenca = feminino - masculino,
          mais_mulher = feminino > masculino) %>%
-  arrange(desc(fem_perc)) %>%
+  arrange(desc(fem_perc)) 
   # filter(mais_mulher == TRUE)
+
+# 2016 + 2020
+eleitorado_2016_2020 <- eleitorado_2020_n %>%
+  left_join(eleitorado_2016_n, by = "cd_municipio") %>%
+  filter(is.na(`total.y`))
