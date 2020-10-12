86e2e8fd77ec32fe07be2f70cf10e1e1689535ed
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-10-12 14:07:52 -0300

    Update recursos_proprios_maior_patrimonio.R

--- prestacao_contas/recursos_proprios_maior_patrimonio.R
+++ prestacao_contas/recursos_proprios_maior_patrimonio.R
@@ -21,7 +21,8 @@ drop_columns <- c("DT_GERACAO", "HH_GERACAO", "CD_TIPO_ELEICAO",
 
 # colunas para selecionar
 select_columns <- c("DT_PRESTACAO_CONTAS", "SQ_PRESTADOR_CONTAS", "NR_CNPJ_PRESTADOR_CONTA",
-                    "SQ_CANDIDATO", "DS_FONTE_RECEITA", "DS_ORIGEM_RECEITA",
+                    "SQ_CANDIDATO", "DS_FONTE_RECEITA", "DS_ORIGEM_RECEITA", "NM_CANDIDATO",
+                    "NR_CPF_CANDIDATO",
                     "DS_NATUREZA_RECEITA", "DS_ESPECIE_RECEITA", "NR_CPF_CNPJ_DOADOR",
                     "NM_DOADOR", "NM_DOADOR_RFB", "DS_ESFERA_PARTIDARIA_DOADOR", "SQ_CANDIDATO_DOADOR", 
                     "DS_CARGO_CANDIDATO_DOADOR", "SG_PARTIDO_DOADOR", "NR_RECIBO_DOACAO",
@@ -34,28 +35,29 @@ class_columns <- c(NR_CPF_CANDIDATO = "character",
                    NR_TITULO_ELEITORAL_CANDIDATO = "character",
                    NR_PROCESSO = "character")
 
-# colunas para definir classe / prestaÁ„o de contas
+# colunas para definir classe / presta√ß√£o de contas
 class_columns_2 <- c(SQ_PRESTADOR_CONTAS = "character", 
                      SQ_CANDIDATO = "character",
                      NR_CPF_CNPJ_DOADOR = "character",
+                     NR_CPF_CANDIDATO = "character",
                      SQ_CANDIDATO_DOADOR = "character",
                      NR_RECIBO_DOACAO = "character",
                      NR_DOCUMENTO_DOACAO = 'character',
                      SQ_RECEITA = "character")
 
 # importing CSV - candidatos
-cand_2020_BR <- fread("C:/Users/acaesar/Downloads/dados_8out2020/consulta_cand_2020/consulta_cand_2020_BRASIL.csv", 
+cand_2020_BR <- fread("C:/Users/acaesar/Downloads/dados_12out2020/consulta_cand_2020/consulta_cand_2020_BRASIL.csv", 
                       encoding = "Latin-1",
                       drop = drop_columns,
                       colClasses = class_columns)
 
 # importing CSV - bens
-bens_2020_BR <- fread("C:/Users/acaesar/Downloads/dados_8out2020/bem_candidato_2020/bem_candidato_2020_BRASIL.csv", 
+bens_2020_BR <- fread("C:/Users/acaesar/Downloads/dados_12out2020/bem_candidato_2020/bem_candidato_2020_BRASIL.csv", 
                       encoding = "Latin-1",
                       colClasses = class_columns)
 
-# importing CSV - prestaÁ„o de contas / CANDIDATOS
-receitas_candidatos <- fread("C:/Users/acaesar/Downloads/dados_8out2020/prestacao_de_contas_eleitorais_candidatos_2020/receitas_candidatos_2020_BRASIL.csv",
+# importing CSV - presta√ß√£o de contas / CANDIDATOS
+receitas_candidatos <- fread("C:/Users/acaesar/Downloads/dados_12out2020/prestacao_de_contas_eleitorais_candidatos_2020/receitas_candidatos_2020_BRASIL.csv",
                              encoding = "Latin-1",
                              select = select_columns,
                              colClasses = class_columns_2)
@@ -63,11 +65,11 @@ receitas_candidatos <- fread("C:/Users/acaesar/Downloads/dados_8out2020/prestaca
 # bens - total
 grouped_bens_2020 <- bens_2020_BR %>%
   mutate(VR_BEM_CANDIDATO = readr::parse_number(VR_BEM_CANDIDATO, 
-                                          locale = readr::locale(decimal_mark = ","))) %>%
+                                                locale = readr::locale(decimal_mark = ","))) %>%
   group_by(SQ_CANDIDATO) %>%
   summarise(total_patrimonio = sum(VR_BEM_CANDIDATO))
 
-# cand _ bens
+# cand - bens
 cand_patrimonio_2020 <- cand_2020_BR %>%
   select(SQ_CANDIDATO, DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO, 
          NR_CPF_CANDIDATO, NR_TITULO_ELEITORAL_CANDIDATO,
@@ -75,22 +77,26 @@ cand_patrimonio_2020 <- cand_2020_BR %>%
   left_join(grouped_bens_2020, by = "SQ_CANDIDATO") %>%
   replace(is.na(.), 0)
 
-# an·lise
+# an√°lise
 recursos_proprios <- receitas_candidatos %>%
   mutate(VR_RECEITA = readr::parse_number(VR_RECEITA, 
                                           locale = readr::locale(decimal_mark = ","))) %>%
-  filter(DS_ORIGEM_RECEITA == "Recursos prÛprios") %>%
-  group_by(SQ_CANDIDATO) %>%
+  filter(DS_ORIGEM_RECEITA == "Recursos pr√≥prios") %>%
+  group_by(SQ_CANDIDATO, NR_CPF_CNPJ_DOADOR, NR_CPF_CANDIDATO,
+           NM_DOADOR, NM_CANDIDATO) %>%
   summarise(recursos_proprios = sum(VR_RECEITA)) %>%
+  mutate(check_doador = NR_CPF_CNPJ_DOADOR == NR_CPF_CANDIDATO) %>%
+  filter(check_doador == TRUE) %>%
+  select(SQ_CANDIDATO, recursos_proprios) %>%
   left_join(cand_patrimonio_2020, by = "SQ_CANDIDATO") %>%
-  mutate(doado_patrimonio = round((recursos_proprios / total_patrimonio) * 100)) %>%
-  filter(doado_patrimonio > 100) %>%
-  filter(doado_patrimonio != "Inf") %>%
-  arrange(desc(recursos_proprios)) %>%
-  select(DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO, SG_UF, NM_UE, SG_UE, 
-         recursos_proprios, total_patrimonio, doado_patrimonio, SQ_CANDIDATO, 
-         NR_TITULO_ELEITORAL_CANDIDATO, NR_CPF_CANDIDATO)
+  mutate(variacao = round((recursos_proprios / total_patrimonio) * 100),
+         diferenca = total_patrimonio - recursos_proprios) %>%
+  filter(diferenca < 0) %>%
+  arrange(desc(diferenca)) %>%
+  ungroup() %>%
+  select(DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO, SG_PARTIDO, SG_UF, NM_UE, SG_UE, 
+         recursos_proprios, total_patrimonio, variacao, diferenca, SQ_CANDIDATO, 
+         NR_TITULO_ELEITORAL_CANDIDATO)
 
 # download
 write.csv(recursos_proprios, "recursos_proprios_patrimonio.csv")
-
