93435933a245e726f6142380a8b34c2a49499ad2
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-10-16 19:27:04 -0300

    Update doacoes_pessoa_fisica.R

--- prestacao_contas/doacoes_pessoa_fisica.R
+++ prestacao_contas/doacoes_pessoa_fisica.R
@@ -34,7 +34,7 @@ class_columns <- c(NR_CPF_CANDIDATO = "character",
                    NR_TITULO_ELEITORAL_CANDIDATO = "character",
                    NR_PROCESSO = "character")
 
-# colunas para definir classe / prestação de contas
+# colunas para definir classe / prestaÃ§Ã£o de contas
 class_columns_2 <- c(SQ_PRESTADOR_CONTAS = "character", 
                      SQ_CANDIDATO = "character",
                      NR_CPF_CNPJ_DOADOR = "character",
@@ -44,34 +44,79 @@ class_columns_2 <- c(SQ_PRESTADOR_CONTAS = "character",
                      SQ_RECEITA = "character")
 
 # importing CSV - candidatos
-cand_2020_BR <- fread("C:/Users/acaesar/Downloads/dados_15out2020/consulta_cand_2020/consulta_cand_2020_BRASIL.csv", 
+cand_2020_BR <- fread("~/Downloads/dados_16out2020/consulta_cand_2020/consulta_cand_2020_BRASIL.csv", 
                       encoding = "Latin-1",
                       drop = drop_columns,
                       colClasses = class_columns)
 
-# importing CSV - prestação de contas / CANDIDATOS - RECEITA
-receitas_candidatos <- fread("C:/Users/acaesar/Downloads/dados_15out2020/prestacao_de_contas_eleitorais_candidatos_2020/receitas_candidatos_2020_BRASIL.csv",
+# importing CSV - prestaÃ§Ã£o de contas / CANDIDATOS - RECEITA
+receitas_candidatos <- fread("~/Downloads/dados_16out2020/prestacao_de_contas_eleitorais_candidatos_2020/receitas_candidatos_2020_BRASIL.csv",
                              encoding = "Latin-1",
                              select = select_columns,
                              colClasses = class_columns_2)
 
 
-# importing CSV - prestação de contas / ÓRGÃO PARTIDÁRIO - RECEITA
-receitas_partidos <- fread("C:/Users/acaesar/Downloads/dados_15out2020/prestacao_de_contas_eleitorais_orgaos_partidarios_2020/receitas_orgaos_partidarios_2020_BRASIL.csv",
-                               encoding = "Latin-1",
-                               select = select_columns,
-                               colClasses = class_columns_2)
+# importing CSV - prestaÃ§Ã£o de contas / Ã“RGÃƒO PARTIDÃRIO - RECEITA
+receitas_partidos <- fread("~/Downloads/dados_16out2020/prestacao_de_contas_eleitorais_orgaos_partidarios_2020/receitas_orgaos_partidarios_2020_BRASIL.csv",
+                           encoding = "Latin-1",
+                           select = select_columns,
+                           colClasses = class_columns_2)
 
+# quantidade de doacoes para candidatos diferentes / PF
+receitas_candidatos_n <- receitas_candidatos %>%
+  mutate(VR_RECEITA= readr::parse_number(VR_RECEITA, 
+                                         locale = readr::locale(decimal_mark = ","))) %>%
+  filter(DS_ORIGEM_RECEITA == "Recursos de pessoas fÃ­sicas") %>%
+  select(SQ_CANDIDATO, NR_CPF_CNPJ_DOADOR, NM_DOADOR, NM_DOADOR_RFB) %>%
+  distinct(SQ_CANDIDATO, NR_CPF_CNPJ_DOADOR, .keep_all = TRUE) %>%
+  mutate(NM_DOADOR_RFB = case_when(str_detect(NM_DOADOR_RFB, "#NULO#") ~ NM_DOADOR,
+                                   !str_detect(NM_DOADOR_RFB, "#NULO#") ~ NM_DOADOR_RFB)) %>%
+  group_by(NR_CPF_CNPJ_DOADOR, NM_DOADOR_RFB) %>%
+  summarise(contagem = n())
+
+# soma de doacoes / PF
+receitas_candidatos_total <- receitas_candidatos %>%
+  mutate(VR_RECEITA= readr::parse_number(VR_RECEITA, 
+                                         locale = readr::locale(decimal_mark = ","))) %>%
+  filter(DS_ORIGEM_RECEITA == "Recursos de pessoas fÃ­sicas") %>%
+  group_by(NR_CPF_CNPJ_DOADOR) %>%
+  summarise(soma = sum(VR_RECEITA))
+
+# total arrecadado por candidato / TOTAL
+receitas_total <- receitas_candidatos %>%
+  mutate(VR_RECEITA= readr::parse_number(VR_RECEITA, 
+                                         locale = readr::locale(decimal_mark = ","))) %>%
+  group_by(SQ_CANDIDATO) %>%
+  summarise(receita_total = sum(VR_RECEITA))
+
+# candidatos 
+cand_2020_BR_n <- cand_2020_BR %>%
+  select(SQ_CANDIDATO, SG_UF, NM_UE, DS_CARGO, NM_CANDIDATO, NM_URNA_CANDIDATO, 
+         NR_CPF_CANDIDATO, DS_SITUACAO_CANDIDATURA, SG_PARTIDO, VR_DESPESA_MAX_CAMPANHA) 
+
+# analise
 t <- receitas_candidatos %>%
   mutate(VR_RECEITA= readr::parse_number(VR_RECEITA, 
-                                                locale = readr::locale(decimal_mark = ","))) %>%
-  filter(DS_ORIGEM_RECEITA == "Recursos de pessoas físicas") %>%
-  group_by(NR_CPF_CNPJ_DOADOR) %>%
-  summarise(valor = sum(VR_RECEITA))
-
-t
-
-
-
-
-
+                                         locale = readr::locale(decimal_mark = ","))) %>%
+  filter(DS_ORIGEM_RECEITA == "Recursos de pessoas fÃ­sicas") %>%
+  group_by(SQ_CANDIDATO, NR_CPF_CNPJ_DOADOR) %>%
+  summarise(int = sum(VR_RECEITA)) %>%
+  arrange(desc(int)) %>% 
+  left_join(receitas_candidatos_n, by = "NR_CPF_CNPJ_DOADOR") %>%
+  left_join(receitas_candidatos_total, by = "NR_CPF_CNPJ_DOADOR") %>%
+  mutate(perc_doacao_total = round((int / soma) * 100)) %>%
+  left_join(cand_2020_BR_n, by = "SQ_CANDIDATO") %>%
+  left_join(receitas_total, by = "SQ_CANDIDATO") %>%
+  mutate(perc_receita = round((int / receita_total)) * 100) %>%
+  mutate(perc_limite = round((int / VR_DESPESA_MAX_CAMPANHA) * 100)) %>%
+  mutate(faixa_doacao = case_when(int > 100000 ~ "acima_de_100_mil",
+                                  int > 50000 & int <= 100000 ~ "de_50_a_100_mil",
+                                  int > 25000 & int <= 50000 ~ "de_25_a_50_mil",
+                                  int > 10000 & int <= 25000 ~ "de_10_a_25_mil",
+                                  int > 5000 & int <= 10000 ~ "de_5_a_10_mil",
+                                  int > 0 & int <= 5000 ~ "de_0_a_5_mil")) %>%
+  select("SQ_CANDIDATO", "NR_CPF_CNPJ_DOADOR", "NM_DOADOR_RFB", "NM_CANDIDATO", "NM_URNA_CANDIDATO", 
+         "SG_PARTIDO", "SG_UF", "NM_UE", "DS_CARGO", "int", "contagem", "soma", "receita_total",
+         "perc_receita", "perc_doacao_total", "perc_limite", "faixa_doacao", "VR_DESPESA_MAX_CAMPANHA") %>%
+  arrange(desc(int))
+  
