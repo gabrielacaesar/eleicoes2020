a2426d565682ecc4be3d331f0554634b4361a917
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-11-02 12:43:06 -0300

    Update cand_abj_processo_criminal.R

--- candidatos/cand_abj_processo_criminal.R
+++ candidatos/cand_abj_processo_criminal.R
@@ -1,27 +1,34 @@
 #HIROTA::
-#A base inclui condenados ou nao? ou soh tramitando?
-#Por que NA? o que faz? eu entro e olho? segredo de justiça?
+#Ver as respostas da Renata
 #Checar caso de estupro de vulneravel ou outro crime mais grave, como homicídio e formação de quadrilha
 
 # leitura de pacotes
 library(tidyverse)
+library(data.table)
 
 # leitura de arquivos / ABJ
-abj_prefeito <- read_rds("C:/Users/acaesar/Downloads/candidatos_HIROTA/prefeitos_criminal.rds")
-abj_vice_prefeito <- read_rds("C:/Users/acaesar/Downloads/candidatos_HIROTA/vice_prefeitos_criminal.rds")
-abj_vereador <- read_rds("C:/Users/acaesar/Downloads/candidatos_HIROTA/vereadores_criminal.rds")
+abj_prefeito <- read_rds("~/Downloads/candidatos_processos_2nov2020/prefeitos_criminal_passivo.rds")
+abj_vice_prefeito <- read_rds("~/Downloads/candidatos_processos_2nov2020/vice_prefeitos_criminal_passivo.rds")
+abj_vereador <- read_rds("~/Downloads/candidatos_processos_2nov2020/vereadores_criminal_passivo.rds")
 
 # leitura de arquivos / CAND
-cand_2020 <- fread("C:/Users/acaesar/Downloads/dados_27out2020/consulta_cand_2020/consulta_cand_2020_BRASIL.csv", 
+cand_2020 <- fread("~/Downloads/dados_2nov2020/consulta_cand_2020_BRASIL.csv", 
                    encoding = "Latin-1",
                    colClasses = c(NR_CPF_CANDIDATO = "character", SQ_CANDIDATO = "character"),
                    select = c("SG_UF", "SG_UE", "NM_UE", "DS_CARGO", "SQ_CANDIDATO",
-                              "NM_CANDIDATO", "NR_CPF_CANDIDATO", "SG_PARTIDO"))
+                              "NM_CANDIDATO", "NR_CPF_CANDIDATO", "SG_PARTIDO", "DS_DETALHE_SITUACAO_CAND",
+                              "NM_EMAIL"))
+
+# ajuste CAND
+cand_2020_n <- cand_2020 %>%
+  filter(DS_DETALHE_SITUACAO_CAND != "RENÚNCIA" &
+           DS_DETALHE_SITUACAO_CAND != "FALECIDO" &
+           DS_DETALHE_SITUACAO_CAND != "CANCELADO")
 
 # análise / PREFEITO
 abj_prefeito_n <- abj_prefeito %>%
-  left_join(cand_2020, by = c("cpf_candidato" = "NR_CPF_CANDIDATO")) %>%
-  group_by(NM_CANDIDATO, SG_PARTIDO, NM_UE) %>%
+  left_join(cand_2020_n, by = c("cpf_candidato" = "NR_CPF_CANDIDATO")) %>%
+  group_by(NM_CANDIDATO, cpf_candidato, SG_PARTIDO, NM_UE, DS_DETALHE_SITUACAO_CAND) %>%
   summarise(int = n()) %>%
   arrange(desc(int))
 
@@ -33,8 +40,8 @@ abj_prefeito_c <- abj_prefeito %>%
 
 # análise / VICE-PREFEITO
 abj_vice_prefeito_n <- abj_vice_prefeito %>%
-  left_join(cand_2020, by = c("cpf_candidato" = "NR_CPF_CANDIDATO")) %>%
-  group_by(NM_CANDIDATO, SG_PARTIDO, NM_UE) %>%
+  left_join(cand_2020_n, by = c("cpf_candidato" = "NR_CPF_CANDIDATO")) %>%
+  group_by(NM_CANDIDATO, cpf_candidato, SG_PARTIDO, NM_UE, DS_DETALHE_SITUACAO_CAND) %>%
   summarise(int = n()) %>%
   arrange(desc(int))
 
@@ -46,8 +53,8 @@ abj_vice_prefeito_c <- abj_vice_prefeito %>%
 
 # análise / VEREADOR
 abj_vereador_n <- abj_vereador %>%
-  left_join(cand_2020, by = c("cpf_candidato" = "NR_CPF_CANDIDATO")) %>%
-  group_by(NM_CANDIDATO, SG_PARTIDO, NM_UE) %>%
+  left_join(cand_2020_n, by = c("cpf_candidato" = "NR_CPF_CANDIDATO")) %>%
+  group_by(NM_CANDIDATO, cpf_candidato, SG_PARTIDO, NM_UE, DS_DETALHE_SITUACAO_CAND) %>%
   summarise(int = n()) %>%
   arrange(desc(int))
 
