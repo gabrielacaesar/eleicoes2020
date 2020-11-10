bde7cb0f343c3cf9780327a7e0928beeca65c0af
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-11-10 18:09:18 -0300

    Update habitantes_por_partido.R

--- apuracao/habitantes_por_partido.R
+++ apuracao/habitantes_por_partido.R
@@ -6,7 +6,7 @@ library(data.table)
 setwd("C:/Users/acaesar/Downloads/populacao/")
 class_columns <- c(cd_uf = "character", cd_ue = "character", pop_est = "character")
 
-hab_2016 <- fread("habitantes_2016_municipios.csv", encoding = "Latin-1", colClasses = class_columns)
+hab_2016 <- fread("habitantes_2016_municipios.csv", encoding = "Latin-1", colClasses = class_columns, stringsAsFactors = FALSE)
 
 # leitura de arquivo / CONVERSOR
 # url: https://github.com/betafcc/Municipios-Brasileiros-TSE/blob/master/municipios_brasileiros_tse.csv
@@ -21,7 +21,7 @@ resultado_2016 <- fread("votacao_candidato_munzona_2016/votacao_candidato_munzon
                                    "DS_SITUACAO_CANDIDATURA", "DS_DETALHE_SITUACAO_CAND", "SG_PARTIDO", "DS_SIT_TOT_TURNO", "QT_VOTOS_NOMINAIS"))
 
 resultado_2016_n <- resultado_2016 %>%
-  filter(DS_ELEICAO == "ELEIÇÕES MUNICIPAIS 2016") %>%
+  filter(DS_ELEICAO == "ELEIÃ‡Ã•ES MUNICIPAIS 2016") %>%
   filter(NR_TURNO == "1") %>%
   filter(DS_CARGO == "Prefeito") %>%
   filter(DS_SIT_TOT_TURNO == "ELEITO") %>%
@@ -30,14 +30,23 @@ resultado_2016_n <- resultado_2016 %>%
   mutate(NM_UE_n = abjutils::rm_accent(toupper(NM_UE)))
 
 resultado_hab_2016 <- hab_2016 %>%
-  filter(nm_ue != "Brasília" &
+  filter(nm_ue != "BrasÃ­lia" &
          nm_ue != "Fernando de Noronha") %>%
   mutate(nm_ue_n = abjutils::rm_accent(toupper(nm_ue)),
          cd_ue = str_pad(cd_ue, 5, pad = 0)) %>%
   unite(cd_ibge, c(cd_uf, cd_ue), sep = "") %>%
   left_join(cod_file, by = c("cd_ibge" = "codigo_ibge")) %>%
   left_join(resultado_2016_n, by = c("codigo_tse" = "CD_MUNICIPIO")) %>%
-  arrange(desc(NM_UE_n))
+  arrange(desc(NM_UE_n)) %>%
+  filter(is.na(NM_UE_n))
 
-write.csv(resultado_hab_2016, "resultado_hab_2016.csv")
+#write.csv(resultado_hab_2016, "resultado_hab_2016.csv")
 
+hab_partido_2016 <- resultado_hab_2016 %>%
+  filter(!is.na(NM_UE_n)) %>%
+  group_by(SG_PARTIDO) %>%
+  summarise(votos = sum(votos_totais),
+            pop_estimada = sum(as.integer(pop_est)),
+            pref_count = n())
+
+#write.csv(hab_partido_2016, "hab_partido_2016.csv")
