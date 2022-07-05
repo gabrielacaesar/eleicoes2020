b5781cd8692dfd9b7095392301c040f48738aeb1
Gabriela Caesar <gabriela.caesar.2019@gmail.com>
2022-07-05 10:03:21 -0300

    Update regiao_df.R

--- frila_2021/scripts/regiao_df.R
+++ frila_2021/scripts/regiao_df.R
@@ -1,7 +1,7 @@
 # criação de DF para converter UF para região
 norte <- data.frame(regiao = "Norte", uf = c("AM", "AC", "AP", "PA", "TO", "RO", "RR"))
 nordeste <- data.frame(regiao = "Nordeste", uf = c("MA", "PI", "CE", "RN", "PE", "PB", "SE", "AL", "BA"))
-centro_oeste <- data.frame(regiao = "Centro-Oeste", uf = c("MT", "MS", "GO"))
+centro_oeste <- data.frame(regiao = "Centro-Oeste", uf = c("MT", "MS", "GO", "DF"))
 sudeste <- data.frame(regiao = "Sudeste", uf = c("SP", "RJ", "ES", "MG"))
 sul <- data.frame(regiao = "Sul", uf = c("RS", "SC", "PR"))
 
